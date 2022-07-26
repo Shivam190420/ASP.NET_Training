fd6bc87b13c01307a481e99615114e473a119db0
Shivam190420 <65591059+Shivam190420@users.noreply.github.com>
2022-07-26 17:13:54 +0530

    Add files via upload

---
+++ Day 1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    { 
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Employee_Details()
+        {
+            return View();
+        }
+
+        public IActionResult Department_Page()
+        {
+            return View();
+        }
+
+        public IActionResult Contact_Us()
+        {
+            return View();
+        }
+
+        public IActionResult About_Us()
+        {
+            return View();
+        }
+
+    }
+}
